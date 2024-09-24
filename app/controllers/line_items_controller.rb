737a23d843c046e923ed6f68bb4e9513ebcca768
Filip Persson <filip.tillberg.persson@gmail.com>
2024-09-24 18:33:03 +0200

    Line items create, carts show

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,24 @@
 class LineItemsController < ApplicationController
   def create
+    product = Product.find(params[:product_id])
+    line_item = current_cart.line_items.find_by(product: product)
+
+    if line_item
+      line_item.quantity += line_item_params[:quantity].to_i
+    else
+      line_item = current_cart.line_items.build(product: product, quantity: line_item_params[:quantity].to_i)
+    end
+
+    if line_item.save
+      redirect_to root_path, notice: "Added to the basket!"
+    else
+      render root_path, alert: "Product could not be added"
+    end
+  end
+
+  private
+
+  def line_item_params
+    params.require(:line_item).permit(:quantity)
   end
 end
