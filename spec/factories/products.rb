e5823a2f32b0fab460cad91ed8142f840fecbf95
Filip Persson <filip.tillberg.persson@gmail.com>
2024-09-24 14:31:33 +0200

    refactored model tests to use factories

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     name { "Sample Product" }
     price { 10.0 }
     product_code { "PRD123" }
+
+    after(:build) do |product|
+      product.photo.attach(
+        io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'default-image.webp')),
+        filename: "default-image.webp",
+        content_type: "image/webp"
+      )
+    end
   end
 end
