7557da342893dee29f4ce51eadf3d6aaca805032
Filip Persson <filip.tillberg.persson@gmail.com>
2024-09-24 15:08:39 +0200

    added grid layout for product index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'line_items/create'
-  get 'carts/show'
-  get 'products/index'
   root to: "products#index"
-  resource :cart, only: :show
+  resources :cart, only: :show
   resources :line_items, only: :create
 end
